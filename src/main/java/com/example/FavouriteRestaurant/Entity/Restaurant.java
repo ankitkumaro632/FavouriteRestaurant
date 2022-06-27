871fcecca8034d0882b2928d01babb6c65a109a4
ankit <ankitkumaro632@gmail.com>
2022-06-27 11:21:42 +0530

    code

--- src/main/java/com/example/FavouriteRestaurant/Entity/Restaurant.java
+++ src/main/java/com/example/FavouriteRestaurant/Entity/Restaurant.java
@@ -24,6 +24,14 @@ public class Restaurant {
 
     private String contact_number;
 
+    private String r_street_no;
+    private String r_address_line1;
+    private String r_address_line2;
+    private String r_city;
+    private String r_postal;
+    private String r_state;
+    private String r_country;
+
     private String hoursOfOperation;
 
     @OneToMany(mappedBy = "restaurant",cascade = CascadeType.ALL,fetch = FetchType.LAZY)
--- src/main/java/com/example/FavouriteRestaurant/Paylods/RestaurantDto.java
+++ src/main/java/com/example/FavouriteRestaurant/Paylods/RestaurantDto.java
@@ -14,6 +14,14 @@ public class RestaurantDto {
 
     private String contact_number;
 
+    private String r_street_no;
+    private String r_address_line1;
+    private String r_address_line2;
+    private String r_city;
+    private String r_postal;
+    private String r_state;
+    private String r_country;
+
     private String hoursOfOperation;
 
 }
